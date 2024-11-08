e19c3414faaeaa62c5c4454dd2956ad14de49cb7
biswaranjans7978 <biswaranjans7978@gmail.com>
2024-11-08 21:46:52 +0530

    image position changes

--- src/components/BannerImage/BannerImage.jsx
+++ src/components/BannerImage/BannerImage.jsx
@@ -4,8 +4,8 @@ function BannerImage() {
   return (
     <>
       <div className=' m-5'>
-        <img className=' h-36 ml-8 border-2 rounded  inset-0 object-cover transition-opacity duration-500 opacity-100 hover:opacity-50' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4AGTCLjmVutWSLP8gwqTAZqwEn2c5HSrnrw&s" alt="" />
-        <img className=' h-36 ml-20 border-2 rounded  inset-0 object-cover transition-opacity duration-500 opacity-100 hover:opacity-50' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTozWOkRiu6-ekpsDiohuj9o2qKeqyVM1Yp9A&s" alt="" />
+        <img className=' h-36 lg:ml-8 border-2 rounded  inset-0 object-cover transition-opacity duration-500 opacity-100 hover:opacity-50' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4AGTCLjmVutWSLP8gwqTAZqwEn2c5HSrnrw&s" alt="" />
+        <img className=' h-36 ml-12 lg:ml-20 border-2 rounded  inset-0 object-cover transition-opacity duration-500 opacity-100 hover:opacity-50' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTozWOkRiu6-ekpsDiohuj9o2qKeqyVM1Yp9A&s" alt="" />
       </div>
     </>
   )
