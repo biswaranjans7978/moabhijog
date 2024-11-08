a0c130c7757fe6a83f7abece56bb394dad6388d2
biswaranjans7978 <biswaranjans7978@gmail.com>
2024-11-08 21:37:37 +0530

    css changes

--- src/components/Abhijogpage/Abhijogpage.jsx
+++ src/components/Abhijogpage/Abhijogpage.jsx
@@ -9,20 +9,20 @@ function Abhijogpage() {
       <div className=" border-2 p-4 mt-4 bg-red-200">
         <CustomerDetails />
       </div>
-      <div className=" h-40 bg-red-200 lg:flex items-center justify-center justify-around pt-10 lg:p-0">
+      <div className=" h-40 bg-red-200 lg:flex items-center justify-center justify-around pt-6 lg:pt-10 pl-4 lg:p-0">
         <div className="">
           <h1>ଏହିଠାରେ ଆପଣଙ୍କ ଅଭିଯୋଗ ଲେଖନ୍ତୁ</h1>
           <textarea className=" w-72" name="" id=""></textarea>
         </div>
         <div className="">କିମ୍ବା</div>
 
-        <div className="">
+        <div className=" bg-red-200 pb-4">
           <ImageUploader />
         </div>
       </div>
       <div className=" flex justify-center">
       <Link to="/thanks">
-        <div className="  bg-zinc-500 w-20   text-white hover:bg-green-600 text-center p-3 border rounded-md">
+        <div className="  bg-zinc-500 w-20   text-white hover:bg-green-600 text-center p-3 border rounded-md mt-14 lg:mt-0">
           Submit
         </div>
       </Link>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <>
-    <div className='h-40 bg-orange-300 grid grid-cols-1 items-center text-center'>
+    <div className=' h-24 lg:h-40 bg-orange-300 grid grid-cols-1 items-center text-center'>
         
             <h2 className=' text-red-800'>ଓଡ଼ିଶା ସରକାର ଆପଣଙ୍କ ସେବାରେ</h2>
         
