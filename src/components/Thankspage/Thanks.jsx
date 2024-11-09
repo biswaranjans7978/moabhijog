3172f7fc2263326e4815eaa6133b07fd3c4e758a
biswaranjans7978 <biswaranjans7978@gmail.com>
2024-11-09 18:20:24 +0530

    new changes

--- src/components/Thankspage/Thanks.jsx
+++ src/components/Thankspage/Thanks.jsx
@@ -7,7 +7,7 @@ function Thanks() {
    <div className=' bg-orange-500 h-96 text-center text-zinc-50 pt-12'>
     <h1 className=' pt-4'>ଆପଣଙ୍କ ଅଭିଯୋଗର ସଙ୍ଖ୍ୟା : </h1>
     <h2 className=' pt-4'>ଆପଣଙ୍କ ଅଭିଯୋଗର ସମାଧାନ ତୁରନ୍ତ କରିବା ପାଇଁଁ ଓଡ଼ିଶା ସରକାର ଯଥା ସମ୍ଭବ ପ୍ରଚେଷ୍ଟା କରିବେ</h2>
-   <Link to="/"> <h3 className=' pt-10'>ଧନ୍ୟବାଦ</h3></Link>
+   <Link to="/"> <button className=' bg-red-600 p-1 m-2 rounded-md hover:bg-blue-600'>ଧନ୍ୟବାଦ</button></Link>
    </div>
    </>
   )
