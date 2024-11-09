96ffed957c7876656b35ec576309136472484da0
biswaranjans7978 <biswaranjans7978@gmail.com>
2024-11-09 18:14:28 +0530

    changes done in customer details page

--- src/components/Showfile/Status.jsx
+++ src/components/Showfile/Status.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Status() {
   return (
     <>
-    <div className=' bg-red-200 h-48 text-center'>
+    <div className=' bg-red-200 h-52 text-center'>
         <div className=' pt-4'>
             <h1>ମୋବାଇଲ୍ ନମ୍ବର୍: <input type="text" /></h1>
             <h1 className=' p-4'>କିମ୍ବା</h1>
             <h1>ଅଭିଯୋଗର ସଙ୍ଖ୍ୟା: <input type="text" /></h1>
-            
-            <div className=' pt-10'><p>ଆପଣଙ୍କ ଅଭିଯୋଗ ଏବେ ପଞ୍ଚାୟତ ଅଫିସ୍ ସ୍ତରରେ ଅଛି...</p></div>
+            <button className=' bg-yellow-400 p-1 mt-2 rounded-md hover:bg-blue-600 '>Submit</button>
+            <div className=' pt-4 '><p>ଆପଣଙ୍କ ଅଭିଯୋଗ ଏବେ ପଞ୍ଚାୟତ ଅଫିସ୍ ସ୍ତରରେ ଅଛି...</p></div>
         </div>
       
     </div>
