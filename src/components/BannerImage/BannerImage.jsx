d538e51e9137c8058d9e6885214997aa0fd45c17
biswaranjans7978 <biswaranjans7978@gmail.com>
2024-11-20 21:00:09 +0530

    image change

--- src/components/BannerImage/BannerImage.jsx
+++ src/components/BannerImage/BannerImage.jsx
@@ -5,7 +5,7 @@ function BannerImage() {
     <>
       <div className=' m-5'>
         <img className=' h-36 lg:ml-8 border-2 rounded  inset-0 object-cover transition-opacity duration-500 opacity-100 hover:opacity-50' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4AGTCLjmVutWSLP8gwqTAZqwEn2c5HSrnrw&s" alt="" />
-        <img className=' h-36 ml-12 lg:ml-20 border-2 rounded  inset-0 object-cover transition-opacity duration-500 opacity-100 hover:opacity-50' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTozWOkRiu6-ekpsDiohuj9o2qKeqyVM1Yp9A&s" alt="" />
+        <img className=' h-36 ml-12 lg:ml-20 border-2 rounded  inset-0 object-cover transition-opacity duration-500 opacity-100 hover:opacity-50' src="https://myblogpod.com/wp-content/uploads/2024/07/Mohan-Majhi-1.webp" alt="" />
       </div>
     </>
   )
